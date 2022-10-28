7ff023613d3492f527730da94f53d8256ca4d4c2
Roy Bellingan <roy@tech.techadsmedia.com>
2022-10-28 19:39:03 +0100

    tips

--- mixin/NoCopy.h
+++ mixin/NoCopy.h
@@ -1,10 +1,15 @@
 #pragma once
 
 class NoCopy {
-      protected:
+	// Note: Scott Meyers mentions in his Effective Modern
+	//C++ book, that deleted functions should generally
+	//be public as it results in better error messages
+	//due to the compilers behavior to check accessibility
+	//before deleted status
+      public:
 	NoCopy()  = default;
 	~NoCopy() = default;
 
-	NoCopy(const NoCopy&) = delete;
+	NoCopy(const NoCopy&)            = delete;
 	NoCopy& operator=(const NoCopy&) = delete;
 };
