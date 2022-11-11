693d0298d7e765b987a2ed80df1ee9892a95e1a4
Roy Bellingan <roy@tech.techadsmedia.com>
2022-11-11 18:05:55 +0000

    newline for debug funkz

--- fmtExtra/echo.cpp
+++ fmtExtra/echo.cpp
@@ -1,9 +1,9 @@
 #include "echo.h"
 #include "fmt/format.h"
 void echo(const std::string_view s) {
-	fmt::print("{}", s);
+	fmt::print("{}\n", s);
 }
 
 void echo(const std::string& s) {
-	fmt::print("{}", s);
+	fmt::print("{}\n", s);
 }
