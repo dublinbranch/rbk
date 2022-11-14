ee93fa2dfaa75382666242481c4539a651fec440
Roy Bellingan <roy@tech.techadsmedia.com>
2022-11-14 16:19:35 +0000

    moar unification

--- locale/codes.h
+++ locale/codes.h
@@ -4,3 +4,5 @@
 #include <QString>
 
 const mapV2<QByteArray, quint16>& getNationISO2();
+const QMap<QString, QString>&     getNationsIsoCodes();
+QString                           getNationIsoCode(const QString& nation);
