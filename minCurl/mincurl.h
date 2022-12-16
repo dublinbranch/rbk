25320f4ae866ced4c4aede87e8bc0047f7ae6895
Roy Bellingan <roy@tech.techadsmedia.com>
2022-12-16 09:51:44 +0000

    adl error in template

--- minCurl/mincurl.h
+++ minCurl/mincurl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "curl/curl.h"
+#include "rbk/filesystem/filefunction.h"
 #include "rbk/mapExtensor/mapV2.h"
 #include "rbk/mixin/NoCopy.h"
 #include <QByteArray>
--- misc/b64.cpp
+++ misc/b64.cpp
@@ -84,14 +84,16 @@ QString base64this(const std::string& param) {
 QString mayBeBase64(const QString& original, bool emptyAsNull) {
 	if (original == SQL_NULL) {
 		return original;
-	} else if (original.isEmpty()) {
+	}
+
+	if (original.isEmpty()) {
 		if (emptyAsNull) {
 			return SQL_NULL;
 		}
 		return QSL("''");
-	} else {
-		return base64this(original);
 	}
+
+	return base64this(original);
 }
 
 QString base64Nullable(const QString& param, bool emptyAsNull) {
