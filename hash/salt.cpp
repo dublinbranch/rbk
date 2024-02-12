8fbcdd92e710a8e574342c7f40d83dafc2415e49
Roy Bellingan <roy@tech.techadsmedia.com>
2024-02-12 13:13:59 +0000

    cry

--- hash/salt.cpp
+++ hash/salt.cpp
@@ -1,8 +1,8 @@
 #include "salt.h"
+#include "rbk/QStacker/exceptionv2.h"
 #include "rbk/misc/b64.h"
 #include "sha.h"
 #include <QDateTime>
-
 std::string salt(std::string, int lenght) {
 	return saltQS(lenght).toStdString();
 }
@@ -12,7 +12,12 @@ QString salt(QString, int lenght) {
 }
 
 QString saltQS(int lenght) {
-	auto ts = QDateTime::currentMSecsSinceEpoch() + rand();
-	auto n  = QByteArray::number(ts);
-	return sha1(n).left(lenght);
+	if (lenght > 86) {
+		throw ExceptionV2("max salt lenght atm is 86, edit the code if you want more");
+	}
+	auto ts  = QDateTime::currentMSecsSinceEpoch() + rand();
+	auto n   = QByteArray::number(ts);
+	auto sha = sha512(n, false);
+	//for *REASON* crypto is retarded, and is not using the standard base64 but they replace 1 character -.- the + with the .
+	return sha1(n).replace("+", ".").left(lenght);
 }
