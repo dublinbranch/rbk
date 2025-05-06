368ffa8f2fcd67a9b5f129f870a732f7d039838d
Roy <roy@6pack>
2025-05-06 20:39:36 +0100

    compile warning

--- filesystem/suffix.cpp
+++ filesystem/suffix.cpp
@@ -1,5 +1,5 @@
 #include "suffix.h"
 
-std::filesystem::path operator"" _p(const char* path, std::size_t) {
+std::filesystem::path operator""_p(const char* path, std::size_t) {
 	return std::filesystem::path(path);
 }
--- filesystem/suffix.h
+++ filesystem/suffix.h
@@ -3,4 +3,4 @@
 #include <filesystem>
 
 // Define a user-defined literal for std::filesystem::path
-std::filesystem::path operator"" _p(const char* path, std::size_t);
+std::filesystem::path operator""_p(const char* path, std::size_t);
